c1d1fd0c85bff88dc9697fd7a973aa0e417c8fa1
Deon Olarewaju <Deonolarewaju@gmail.com>
2023-11-11 13:41:06 +0100

    created mockbasemodelclass

--- src/_core/__test__/base.service.spec.ts
+++ src/_core/__test__/base.service.spec.ts
@@ -3,5 +3,8 @@ import { BaseService } from '../_base';
 ​
 
 export class MockBaseModelClass {
+    public static collection = { collectionName: 'MockAttributeModel' };
+    public static config = () => {
 
+    };
 }
