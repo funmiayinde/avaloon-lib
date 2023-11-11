35ad9e075e9db37b326949f49102ea0a2b791d52
Deon Olarewaju <Deonolarewaju@gmail.com>
2023-11-11 13:42:27 +0100

    created config and collection mock

--- src/_core/__test__/base.service.spec.ts
+++ src/_core/__test__/base.service.spec.ts
@@ -5,6 +5,13 @@ import { BaseService } from '../_base';
 export class MockBaseModelClass {
     public static collection = { collectionName: 'MockAttributeModel' };
     public static config = () => {
-
-    };
+        return {
+          fillables: ['name'],
+          updateFillables: ['name'],
+          uniques: ['name'],
+          dateFilters: ['createdAt', 'updatedAt'],
+          softDelete: true,
+        };
+      };
+      data: any;
 }
