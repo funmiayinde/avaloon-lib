6b6dc2193c168785d9f88d311ce37858e4aab1d0
Deon Olarewaju <Deonolarewaju@gmail.com>
2023-11-14 12:55:27 +0100

    added expected behaviour, and completed test on should add hours to current date using default value.

--- src/_core/__test__/add-hour-to-date.spec.ts
+++ src/_core/__test__/add-hour-to-date.spec.ts
@@ -7,7 +7,11 @@ describe('addHourToDate', () => {
 ​
     const result = Utils.addHourToDate();
 ​
-   
+    expect(generateNewdateSpy).toHaveBeenCalled();
+    expect(generateNewdateSpy).toHaveBeenCalledWith();
+    expect(generateNewdateSpy).toHaveReturned();
+    expect(generateNewdateSpy).toHaveReturnedWith(result);
+    expect(result instanceof Date).toBe(true);
 ​
     generateNewdateSpy.mockClear();
   });
